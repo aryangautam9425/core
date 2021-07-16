cc668fd7fd94be58b5359a0d77155d4da7095e56
aryangautam9425 <68505143+aryangautam9425@users.noreply.github.com>
2021-07-16 20:41:43 +0530

    Add files via upload

---
+++ DotNetCoreConsoleApplication/DotNetCoreConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCoreConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
